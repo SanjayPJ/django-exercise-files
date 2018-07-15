84be766c7b2d16b737629d514eb5a906d557e096
SanjayPJ <32980569+SanjayPJ@users.noreply.github.com>
2018-07-16 00:09:59 +0530

    added notes.py to python-exercise files"

---
+++ python-exercise-files/notes.py
@@ -0,0 +1,22 @@
+
+
+# boolean values
+
+isTrue = True
+
+isFalse = False
+
+print(isTrue ,isFalse)
+
+# tuple
+
+my_tuple = (45, "something", [45,12])
+
+print(my_tuple)
+
+
+#set
+
+my_set = {1,2,"name"}
+
+print(my_set)
