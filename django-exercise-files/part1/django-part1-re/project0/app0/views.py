1bf10b2868051df3298b09d82086c229530b844d
bingo6 <bingo6@localhost.localdomain>
2018-07-24 16:38:59 +0530

    added full part 1 re

---
+++ django-exercise-files/part1/django-part1-re/project0/app0/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    dict1 = {
+        'name': 'sanjaypj',
+        'age': 12,
+    }
+    return render(request, 'index.html', dict1)
---
+++ django-exercise-files/part1/django-part1-re/project0/project0/urls.py
@@ -0,0 +1,36 @@
+"""project0 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    from django.urls import path, re_path
+
+from . import views
+
+urlpatterns = [
+    path('articles/2003/', views.special_case_2003),
+    re_path(r'^articles/(?P<year>[0-9]{4})/$', views.year_archive),
+    re_path(r'^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive),
+    re_path(r'^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$', views.article_detail),
+]
+
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from django.urls import include, path
+
+urlpatterns = [
+    path('app0/', include('app0.urls')),
+    path('app1/', include('app1.urls')),
+    path('admin/', admin.site.urls),
+]
