68d432128ff3c526b433b307840f578ee42ee487
sanjaypj <sanjaypjayan2000@gmail.com>
2018-08-25 01:42:22 +0530

    db included

--- django-exercise-files/part2/first_project/first_app/views.py
+++ django-exercise-files/part2/first_project/first_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
+
+def index(request):
+    AccessRecordList = AccessRecord.objects.order_by("date")
+    return render(request, "index.html", context={
+        "access_records" : AccessRecordList
+        })
